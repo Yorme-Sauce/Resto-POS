2852a73cefb987d3713d933a695a82e45b8758e4
devstygian <hackstygian@gmail.com>
2026-03-18 21:32:05 +0800

    chore: restructure folder
    Organize pages and files for cleaner project structure

--- M_management/delete_menu.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../config/config.php';
-
-$id = $_GET['id'];
-
-$conn->query("DELETE FROM menu WHERE menuID='$id'");
-
-header("Location: menu_management.php");
-?>
--- order/delete_order.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "ordering_system");
-
-// Use orderID instead of id
-$orderID = intval($_GET['id']);
-
-$conn->query("DELETE FROM orders WHERE orderID=$orderID");
-echo "Order deleted successfully!";
