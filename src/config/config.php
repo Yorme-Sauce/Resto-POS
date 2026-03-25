72cf05041c553851131d236ffa78b803da67aa43
devstygian <hackstygian@gmail.com>
2026-03-25 11:26:07 +0800

    refactor: add debuger to show error for AJAX

--- src/config/config.php
+++ src/config/config.php
@@ -1,5 +1,8 @@
 <?php
-// Start session only if not already started
+// SILENCE ERRORS FOR AJA
+error_reporting(0);
+ini_set('display_errors', 0);
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
